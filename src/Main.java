57d8c1728efdd6c8accf7559cf0463a12ba015a5
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-10 02:38:14 -0300

    Add info.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+/* IMPORTANT: An ArrayList STORES OBJECTS >>>> STRING, INTEGER, CHARACTER, ETC ETC!!!!!!!!!!!!
+ *
+ * The ArrayList class is a resizable array, which can be found in the java.util package.
+ * The difference between a built-in array and an ArrayList in Java, is that the size of
+ * an array cannot be modified (if you want to add or remove elements to/from an array,
+ * you have to create a new one).
+ * While elements can be added and removed from an ArrayList whenever you want.
+ * The syntax is also slightly different.
+ * Elements in an ArrayList are actually objects. In the examples above, we created elements
+ * (objects) of type "String". Remember that a String in Java is an object (not a primitive
+ * type).
+ *
+ * SOURCE: https://www.w3schools.com/java/java_arraylist.asp
+ */
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
